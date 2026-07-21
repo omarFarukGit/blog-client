5d2568d1bfff5721e79ff6b9e078ddd1a2758514
practiceTaqwa <muhammadabuhafz@gmail.com>
2026-07-21 10:31:42 +0600

    catch all routes in proxy file

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
-
-export function proxy(request:NextRequest){
-    console.log('proxy')
-    return NextResponse.redirect(new URL('/',request.url))
+export function proxy(request: NextRequest) {
+  console.log("proxy");
+  return NextResponse.redirect(new URL("/", request.url));
 }
 
-export const config={
-    matcher:['/dashboard/:path*','/author-dashboard/:path*']
-}
+export const config = {
+  matcher: [
+    // "/dashboard/:path*", "/author-dashboard/:path*"
+    "/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)",
+  ],
+};
