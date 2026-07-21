56b44d6c723f6e4491dfbaabb377cb3bc20a2a40
practiceTaqwa <muhammadabuhafz@gmail.com>
2026-07-21 10:22:08 +0600

    added proxy file

---
+++ src/proxy.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+export function proxy(request:NextRequest){
+    console.log('proxy')
+    return NextResponse.redirect(new URL('/',request.url))
+}
+
+export const config={
+    matcher:['/dashboard/:path*','/author-dashboard/:path*']
+}
