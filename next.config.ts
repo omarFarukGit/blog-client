47d48ab22e2857fb0dad2cdc84ae58f95e69f446
practiceTaqwa <muhammadabuhafz@gmail.com>
2026-07-23 11:56:45 +0600

    Image Component In NextJS

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  cacheComponents:true
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+        port: "",
+        pathname: "/account123/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
